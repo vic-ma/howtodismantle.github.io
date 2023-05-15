64ebbb7bd8acdaf2f1a6d4ab7a69f1caf5799ec2
Erwin <erwin@remotedev.co>
2023-05-15 19:50:11 +0200

    first version

--- _posts/2023-05-15-circuit-overview.md
+++ _posts/2023-05-15-circuit-overview.md
@@ -1,8 +1,9 @@
 ---
 layout: post
-title:  "Welcome to Jekyll!"
-date:   2023-05-14 22:35:52 +0200
-categories: jekyll update
+title: Circuit overview
+date: 2023-05-15 00:35:52 +0200
+tags: hardware
+image: /assets/img/example-img.jpeg
 ---
 You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,30 +8,64 @@ module.exports = {
         './*.html',
     ],
     theme: {
-        theme: {
-            extend: {},
-            fontFamily: {
-                title: ['HelveticaDisplayBlack'],
-                regular: ['HelveticaNow'],
-                sans: [
-                    "HelveticaNow",
-                    "sans-serif"
-                ],
-                serif: [
-                    "Roboto",
-                    "Georgia",
-                    "serif"
-                ],
-                mono: [
-                    "Menlo",
-                    "Monaco",
-                    "Consolas",
-                    "Liberation Mono",
-                    "Courier New",
-                    "monospace"
-                ]
-            },
+        extend: {
+            typography: ({ theme }) => ({
+                dismantle: {
+                    css: {
+                        '--tw-prose-body': theme('colors.gray[200]'),
+                        '--tw-prose-headings': theme('colors.white'),
+                        '--tw-prose-lead': theme('colors.gray[300]'),
+                        '--tw-prose-links': theme('colors.secondary'),
+                        '--tw-prose-bold': theme('colors.white'),
+                        '--tw-prose-counters': theme('colors.gray[400]'),
+                        '--tw-prose-bullets': theme('colors.gray[600]'),
+                        '--tw-prose-hr': theme('colors.gray[700]'),
+                        '--tw-prose-quotes': theme('colors.gray[100]'),
+                        '--tw-prose-quote-borders': theme('colors.gray[700]'),
+                        '--tw-prose-captions': theme('colors.gray[400]'),
+                        '--tw-prose-code': theme('colors.white'),
+                        '--tw-prose-pre-code': theme('colors.gray[300]'),
+                        '--tw-prose-pre-bg': 'rgb(0 0 0 / 50%)',
+                        '--tw-prose-th-borders': theme('colors.gray[600]'),
+                        '--tw-prose-td-borders': theme('colors.gray[700]'),
+                        strong: {
+                            'font-weight': 700,
+                        },
+                    },
+                }
+            }),
+            colors: {
+                blackground: '#1C1C1C',
+                blueground: '#202C3C',
+                primary: '#143761',
+                secondary: '#21AFE1',
+                light: '#ECEBE4',
+            }
+        },
+        fontFamily: {
+            title: ['HelveticaDisplayBlack'],
+            regular: ['HelveticaNow'],
+            sans: [
+                "HelveticaNow",
+                "sans-serif"
+            ],
+            serif: [
+                "Roboto",
+                "Georgia",
+                "serif"
+            ],
+            mono: [
+                "Menlo",
+                "Monaco",
+                "Consolas",
+                "Liberation Mono",
+                "Courier New",
+                "monospace"
+            ]
         },
     },
-    plugins: []
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
+    ],
 }
